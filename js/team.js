4262dd5938319a679b4327acf65a154bd6643095
Riamarh <kiriam@bk.ru>
2021-11-16 17:29:35 +0300

    Animated scroll

--- js/team.js
+++ js/team.js
@@ -1,2 +1,12 @@
 $('.parallax-window').parallax({imageSrc: 'img/intro.jpg'});
 
+$(function(){
+    $('.nav-link').click(function(){
+        var id = $(this).attr('href');
+
+        $('html, body').animate({
+            scrollTop: ($(id).offset().top - 76)
+        }, 500);
+        return false;
+    });
+});
