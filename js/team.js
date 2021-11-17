57aaae85fbeeaa9a7e568ed65ab95a643b03319a
Riamarh <kiriam@bk.ru>
2021-11-17 15:47:14 +0300

    New indents

--- js/team.js
+++ js/team.js
@@ -1,12 +1,12 @@
 $('.parallax-window').parallax({imageSrc: 'img/intro.jpg'});
 
 $(function(){
-    $('.nav-link').click(function(){
-        var id = $(this).attr('href');
+  $('.nav-link').click(function(){
+    var id = $(this).attr('href');
 
-        $('html, body').animate({
-            scrollTop: ($(id).offset().top - 76)
-        }, 500);
-        return false;
-    });
+    $('html, body').animate({
+      scrollTop: ($(id).offset().top - 76)
+    }, 500);
+    return false;
+  });
 });
